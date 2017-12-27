172dc5b6d66e68662dbfa440c46c6b1a550ad54d
ykiwmy <1293089043@qq.com>
2017-12-27 23:00:33 +0800

    excel modify2

--- PycharmProjects/Excel/Excel1.py
+++ PycharmProjects/Excel/Excel1.py
@@ -3,22 +3,40 @@ import xlwt
 import struct
 
 
-def GetSelfName():
-    return
-def read_excel(sheetname):
-    readbook = xlrd.open_workbook('test.xlsx')  # 打开文件
-    sheet = readbook.sheet_by_name(sheetname)  # 通过名称获取
-    nrows = sheet.nrows  # 获取sheet的行数
-    ncols = sheet.ncols  # 获取sheet的列数
-    print(nrows, ncols)
+readbook = xlrd.open_workbook('test.xlsx')  # 打开文件
 
-    selfname = sheet.cell(0, 1).value
-    print(selfname)
+# 通过名称获取sheet
+def GetSheetByName(sheetname):
+    return readbook.sheet_by_name(sheetname)
+
+# 通过名称获取行数
+def GetRowsByName(sheetname):
+    sheet = GetSheetByName(sheetname)
+    return sheet.nrows  # 获取sheet的行数
+
+# 通过名称获取列数
+def GetColsByName(sheetname):
+    sheet = GetSheetByName(sheetname)
+    return sheet.ncols  # 获取sheet的行数
+
+# 获取自身站名
+def GetSelfName(sheetname):
+    sheet = GetSheetByName(sheetname)
+    return sheet.cell(0, 1).value
+
+def GetInfo(sheetname,selfname):
+    nrows = GetRowsByName(sheetname)
+    sheet = GetSheetByName(sheetname)
     for i in range(1,nrows):
         name = sheet.cell(i, 0).value
-        IxL = sheet.cell(i, 1).value
-        TGMT = sheet.cell(i, 2).value
-        print(name, IxL, TGMT)
+        if name == selfname:
+            IxL = sheet.cell(i, 1).value
+            TGMT = sheet.cell(i, 2).value
+            #print(name, IxL, TGMT)
+            return(name,IxL,TGMT)
+
+SelfName = GetSelfName('SelfInfo')
+print(SelfName)
+Info = GetInfo('Sahara',SelfName)
 
 
-read_excel('Sheet1')
